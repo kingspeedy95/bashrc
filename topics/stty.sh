2760c7ea09038380b70f163fb212854a8a304234
Malte Hoffmann <andi.arbait@gmail.com>
2024-08-28 11:30:51 +0200

    Update stty.sh for VirtualBox

--- topics/stty.sh
+++ topics/stty.sh
@@ -1,4 +1,5 @@
 # Use ^X instead of ^S to stop control flow so I can reclaim ^S for forward
 # common history search
+if [ $TERM == linux ]; then unicode_start; fi
 stty -F /dev/tty -ixon || stty -f /dev/tty -ixon
 stty -F /dev/tty stop ^X || stty -f /dev/tty stop ^X
